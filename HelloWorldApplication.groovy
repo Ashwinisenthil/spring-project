727bd92d94904eab4474857944becbc9aedadc03
Senthilkumar Gandhirajan <VGSenthil@V2Mac.local>
2020-11-20 09:48:03 -0500

    second commit

---
+++ HelloWorldApplication.groovy
@@ -0,0 +1,16 @@
+package com.first.project.Hello.World
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class HelloWorldApplication {
+
+	static void main(String[] args) {
+		SpringApplication app = new SpringApplication(HelloWorldApplication.class);
+		app.setDefaultProperties(Collections
+				.singletonMap("server.port", "8084"));
+		app.run(args);
+	}
+
+}
